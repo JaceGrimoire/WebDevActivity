8ec0bbf9ac4d4f669d276ed1a96cc0930ad02313
JaceGrimoire <73150051+JaceGrimoire@users.noreply.github.com>
2020-12-12 11:03:05 +0800

    Added Pop-up nav bar on scroll

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
  $(document).ready(function() {
     $('#imageSet1').lightSlider({
         autoWidth:true,
-        loop:true,
+        loop:false,
         pager:true,
         onSliderLoad: function() {
             $('#imageSet1').removeClass('cS-hidden');
@@ -12,10 +12,21 @@
  $(document).ready(function() {
     $('#imageSet2').lightSlider({
         autoWidth:true,
-        loop:true,
+        loop:false,
         pager:true,
         onSliderLoad: function() {
             $('#imageSet2').removeClass('cS-hidden');
         } 
     });  
   });
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
